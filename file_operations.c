dba794d5a14a39882748f3d1cd9231224a7ea1c8
Sean <82419183+MajenBej@users.noreply.github.com>
2025-09-16 01:15:49 +0000

    finished homework

--- file_operations.c
+++ file_operations.c
@@ -6,58 +6,80 @@
 #include "file_operations.h"
 
 int create_and_write_file(const char *filename, const char *content) {
-    // TODO: Declare an integer 'fd' for the file descriptor.
-    // TODO: Declare a variable 'bytes_written' of type ssize_t to store how many bytes are written.
+    int fd;
+    ssize_t bytes_written;
 
-    // TODO: Print a message showing which file is being created.
-    // TODO: Print a message showing what content will be written.
+    printf("Creating file %s \n", filename);
+    printf("Written Content %s \n", content);
 
-    // TODO: Open or create the file for writing using the open() system call.
-    // TODO: Use flags O_CREAT | O_WRONLY | O_TRUNC and permissions 0644.
-    // TODO: Check if open() failed (fd == -1). If so, print an error using perror and return -1.
+    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 
-    // TODO: Print the file descriptor value.
+    if (fd == -1) {
+        perror("open failed");
+        return -1;
+    }
 
-    // TODO: Write the content to the file using the write() system call.
-    // TODO: Use the length of 'content' as the size to write.
-    // TODO: Check if write() failed (bytes_written == -1). If so, print an error using perror, close the file, and return -1.
+    printf("File Descripter %d \n", fd);
 
-    // TODO: Print a success message with the number of bytes written and the filename.
+    bytes_written = write(fd, content, strlen(content));
 
-    // TODO: Close the file using close(fd).
-    // TODO: Check if close() failed. If so, print an error using perror and return -1.
+   if (bytes_written == -1) {
+        perror("writing failed");
+        close(fd);
+        return -1;
+   }
 
-    // TODO: Print a message that the file was closed successfully.
+   printf("Successfully created/wrote file %s with bytes %zd", filename, bytes_written);
+
+   fd = close(fd);
+
+   if (fd == -1) {
+        return -1;
+   }
+
+    printf("File closed successfully!");
+
     return 0;
 }
 
 int read_file_contents(const char *filename) {
-    // TODO: Declare an integer 'fd' for the file descriptor.
-    // TODO: Create a buffer array of size 1024 to store the file data.
-    // TODO: Declare a variable 'bytes_read' of type ssize_t to store how many bytes are read.
 
-    // TODO: Print a message showing which file is being read.
+    int fd;
+    char buffer[1024];
+    ssize_t bytes_read; 
 
-    // TODO: Open the file for reading using the open() system call.
-    // TODO: Use the O_RDONLY flag.
-    // TODO: Check if open() failed (fd == -1). If so, print an error using perror and return -1.
+    printf("%s is being read!\n", filename);
 
-    // TODO: Print the file descriptor value.
-    // TODO: Print a header for the file contents.
+    fd = open(filename, O_RDONLY);
 
-    // TODO: Read the file contents using the read() system call in a loop.
-    // TODO: Use sizeof(buffer) - 1 for the buffer size.
-    // TODO: Null-terminate the buffer after each read.
-    // TODO: Print the contents of the buffer.
-    // TODO: Continue reading until read() returns 0.
+    if (fd == -1) {
+        perror("open failed!");
+        return -1;
+    }
 
-    // TODO: Check if read() failed (bytes_read == -1). If so, print an error using perror, close the file, and return -1.
+    printf("File Descriptor %d \n", fd);
+    printf("----- FILE CONTENTS -----\n");
 
-    // TODO: Print a footer for the end of the file.
 
-    // TODO: Close the file using close(fd).
-    // TODO: Check if close() failed. If so, print an error using perror and return -1.
+    while ((bytes_read = read(fd, buffer, sizeof(buffer) - 1)) > 0) {
+        buffer[bytes_read] = '\0'; 
+        printf("%s", buffer); 
+    }
 
-    // TODO: Print a message that the file was closed successfully.
+    if (bytes_read == -1) {
+        perror("failed to read bytes!");
+        return -1;
+    }
+
+    printf("\n----- END OF FILE -----\n");
+
+   fd = close(fd);
+
+   if (fd == -1) {
+    perror("failed to close file");
+    return -1;
+   }
+
+   printf("File was clsoed successfully!");
     return 0;
 }
